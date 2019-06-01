2439befc431d084437a3e24434f53b69be2ac4c6
Eliott <Leskigkrem@gmail.com>
2019-06-01 21:36:57 +0200

    prefabus

---
+++ Fish/Assets/Yeet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yeet : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+        
+    }
+}
