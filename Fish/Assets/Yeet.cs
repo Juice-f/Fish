8d9d2c8cd05a508bd5bf753a38f4e363d23548b3
Eliott <Leskigkrem@gmail.com>
2019-06-01 22:38:42 +0200

    Fun stuff

--- Fish/Assets/Yeet.cs
+++ Fish/Assets/Yeet.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Yeet : Interactable
+
 {
+    public float kickstrength = 10;
     public override void Interact()
     {
         base.Interact();
-        
+        GetComponent<Rigidbody>().AddForce(new Vector3(25, 25) * kickstrength);
+
+
     }
+    
 }
