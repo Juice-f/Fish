7d405b139ee22036ba45accd316ff176d30206d6
Eliott <Leskigkrem@gmail.com>
2019-06-01 11:55:49 +0200

    BUDGET MOVEMENT
    BUDGET MOVEMENT

---
+++ Fish/Assets/TutorialInfo/Interactable.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+    bool isfocus = false;
+    bool hasInteracted = false;
+    Transform player;
+
+    public virtual void Interact()
+    {
+        //this method is meant to be overwritten
+        Debug.Log("interacting with" + transform.name);
+    }
+    private void Update()
+    {
+        if (isfocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isfocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OndeFocused()
+    {
+        isfocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
+
+        
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
