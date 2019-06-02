fb3dc78868350b825d8099a48874b00626245733
Eliott <Leskigkrem@gmail.com>
2019-06-02 21:59:43 +0200

    Instructions

---
+++ Fish/Assets/Instructions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour
+{
+
+    public void Backbutton()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
